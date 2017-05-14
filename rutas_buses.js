a69327323363be293e1e7684b9a4d604c23d1b1e
Marvin Barahona <marosvramirez@gmail.com>
2017-05-14 04:51:17 -0600

    Vista inicial, solo con las dos capas base

---
+++ rutas_buses.js
@@ -0,0 +1,31 @@
+function init() {
+  // Creando la capa de OSM
+  var capaOSM = new ol.layer.Tile({source: new ol.source.OSM()});
+  var capaParadas = crearCapa("paradas");
+  var capaCalles = crearCapa("calles");
+
+  //Creando la vista del mapa
+  var view = new ol.View({
+    projection: 'EPSG:4326',
+    center: [-89.18015, 13.73789],
+    zoom: 11.8
+  });
+
+  var map = new ol.Map({
+    layers: [capaOSM, capaCalles, capaParadas],
+    view: view,
+    target: 'map'
+  });
+}
+
+function crearCapa(nombreCapa){
+  var nombre = "RutasBuses:" + nombreCapa
+  var wms =  new ol.source.TileWMS({
+    url: 'http://localhost:8080/geoserver/RutasBuses/wms',
+    params: {
+      'LAYERS': nombre
+    }
+  });
+
+  return new ol.layer.Tile({source: wms});
+}
